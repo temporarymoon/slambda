9105e64e50fd4398555ff0a85113dcc44daed7c3
Matei Adriel <rafaeladriel11@gmail.com>
2022-04-06 00:29:01 +0300

    feat: working chords

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import evdev, asyncio
+from enum import Enum, IntEnum
 from evdev.events import InputEvent, KeyEvent, SynEvent
 from evdev.ecodes import keys, KEY, SYN, REL, ABS, EV_KEY, EV_REL, EV_ABS, EV_SYN, KEY_A, KEY_J, KEY_P
 
@@ -15,70 +16,176 @@ for device in allDevices:
 
 print("\nEvents:")
 # deviceCodes = [10, 20] # both keyboards
-deviceCodes = [] 
+deviceCodes = [7] 
 devicePaths = ["/by-path/platform-i8042-serio-0-event-kbd"] + [f"event{code}" for code in deviceCodes] # laptop only
 devices = [evdev.InputDevice(f"/dev/input/{path}") for path in devicePaths]
 
-keyState = {}
+# keyState = {}
+# 
+# previous = None
+# 
+# chordMax = 1/100 # ms
+# currentChord = []
+# 
+# 
+# for device in devices:
+#     device.grab()
 
-previous = None
+ec = evdev.ecodes
+blacklisted = [ec.KEY_LEFTSHIFT]
+basicRemaps = [
+    dict(from_ = [ec.KEY_D, ec.KEY_S], to = [ec.KEY_LEFTSHIFT]),
+    dict(from_ = [ec.KEY_L, ec.KEY_K], to = [ec.KEY_RIGHTSHIFT])
+]
 
-chordMax = 1/100 # ms
-currentChord = []
+def mapChord(chord):
+    if len(chord) <= 1:
+        return
 
-ui = evdev.UInput(name="My-own-sink")
+    print(*[event.keycode for event in chord])
+
+    codes = [keyEvent.event.code for keyEvent in chord]
+
+    for remap in basicRemaps:
+        if not all(code in codes for code in remap["from_"]):
+            continue
+        return remap["to"] + [code for code in codes if code not in remap["from_"]]
+
+    print("No mapping found")
+
+    return None # default to the unsorted chord
+
+class DeviceManager:
+    delay = 1/30 # in seconds
+
+    def __init__(self, device, ui = None):
+        self.device = device
+        self.taskEndChord = None
+        self.currentChord = []
+        self.pressedCombos = []
+        self.ui = ui
+
+        if ui != None:
+            self.device.grab()
+
+    def writeUi(self, type, code, value, fallback = None):
+        if self.ui == None: 
+            return print(
+                self.device.path, 
+                fallback if fallback != None else f"Type: {type}, Code: {code}, value: {value}", 
+                sep = ": "
+            )
+
+        self.ui.write(type, code, value)
+        self.ui.syn()
+
+    def sendEvent(self, event, printEvent = None):
+        if isinstance(event, KeyEvent):
+            return self.writeUi(event.event.type, event.event.code, event.event.value, event)
+
+        self.writeUi(event.type, event.code, event.value, event)
+
+    def sendKey(self, key, value):
+        _up = "up"
+        _down = "down"
+        self.writeUi(EV_KEY, key, value, f"Key: {evdev.ecodes.keys[key]} {_down if value else _up}")
+
+    async def handlePressDelay(self, event):
+        await asyncio.sleep(DeviceManager.delay)
+
+        # Hooray, we haven't been cancelled yet! Time to end the chord I guess
+        self.taskEndChord = None # hide the evidence :p
+
+        chord = self.currentChord
+        sortedChord = sorted(chord, key = lambda e: e.keycode)
+        mapped = mapChord(chord)
+
+        if mapped == None: # No remapping found :(
+            for event in chord:
+                self.sendEvent(event) # Send the original events through
+        else: # Mapping found!
+            for key in mapped: # Press all the remapped keys
+                self.sendKey(key, 1)
+
+            # Remember the chord for the keyup events
+            combo = dict(keys = [], mappedTo = mapped)
+
+            self.pressedCombos.append(combo)
+
+            for event in self.currentChord:
+                combo["keys"].append(dict(isPressed = True, event = event))
+
+        self.currentChord.clear()
+
+    def handleEvent(self, event):
+        if event.type != EV_KEY:
+            return
+
+        if event.code in blacklisted:
+            return
+
+        categorized = evdev.categorize(event)
+        # print(self.device.path, categorized, sep=': ')
+
+        if categorized.keystate == KeyEvent.key_down:
+            if self.taskEndChord != None:
+                self.taskEndChord.cancel()
+
+            self.currentChord.append(categorized)
+            self.taskEndChord = asyncio.create_task(self.handlePressDelay(categorized))
+        elif categorized.keystate == KeyEvent.key_up:
+            for combo in self.pressedCombos:
+                keyData = None
+                keys = combo["keys"]
+                for key in keys:
+                    if key["event"].event.code == event.code and key["isPressed"]:
+                        keyData = key
+                        break
+
+                if keyData != None:
+                    keyData["isPressed"] = False
+                    if not any([key["isPressed"] for key in keys]):
+                        self.pressedCombos.remove(combo)
+                        for key in combo["mappedTo"]:
+                            self.sendKey(key, 0)
+                    break
+            else:
+                self.sendEvent(categorized)
+        elif categorized.keystate == KeyEvent.key_hold:
+            print("Holding has not yet been implemented!")
+
+    async def startLoop(self):
+        async for event in device.async_read_loop():
+            try: 
+                self.handleEvent(event)
+            except (Exception, KeyboardInterrupt) as e:
+                print("An error occured: ", e)
+                # Handle errors :D
+                if self.loop != None:
+                    loop.stop()
+
+                return
+managers = []
+ui = evdev.UInput(name = "My python uinput!")
 
 for device in devices:
-    device.grab()
+    manager = DeviceManager(device, ui = ui)
+    managers.append(manager)
+    asyncio.ensure_future(manager.startLoop())
 
-async def print_events(device):
-    global currentChord
-    async for event in device.async_read_loop():
-        try:
-            categorized = evdev.categorize(event)
-            foundChord = False
-            if event.type == 4:
-                ui.write(event.type, event.code, event.value)
-            elif event.type == EV_KEY:
-                if categorized.keystate == KeyEvent.key_down:
-                    if len(currentChord) == 0:
-                        currentChord.append(event)
-                    elif (event.timestamp() - currentChord[-1].timestamp()) <= chordMax:
-                        currentChord.append(event)
-                    else:
-                        currentChord.clear()
-                        currentChord.append(event)
-                    keyState[categorized.keycode] = True
-                elif categorized.keystate == KeyEvent.key_up:
-                    if len(currentChord) > 1 and categorized.keycode == evdev.categorize(currentChord[-1]).keycode:
-                        print("Chord detected: ", [evdev.categorize(event).keycode for event in currentChord])
-                        currentChord = sorted(currentChord, key = lambda e: evdev.categorize(e).keycode)
-                        if len(currentChord) == 2 and currentChord[0].code == KEY_A and currentChord[1].code == KEY_J:
-                            print("Sending chord!")
-                            ui.write(EV_KEY, KEY_P, 1)
-                            ui.syn()
-                            ui.write(EV_KEY, KEY_P, 0)
-                            ui.syn()
-                        currentChord.clear()
-                        foundChord = True
-                    keyState[categorized.keycode] = False
-                ui.write(event.type, event.code, event.value)
-            elif event.type == EV_SYN and not (previous.type == EV_KEY and evdev.categorize(previous).keystate == KeyEvent.key_hold):
-                if not foundChord:
-                    print(f"Pressed keys: {[code for (code, pressed) in keyState.items() if pressed]}")
-                else:
-                    print("\n")
-                ui.syn()
+def custom_exception_handler(loop, context):
+    # first, handle with default handler
+    loop.default_exception_handler(context)
 
-            previous = event
-            # else:
-            #     print(device.path, categorized, sep=': ')
-        except:
-            exit()
+    exception = context.get('exception')
 
-
-for device in devices:
-    asyncio.ensure_future(print_events(device))
+    print(context)
+    loop.stop()
 
 loop = asyncio.get_event_loop()
+
+for manager in managers:
+    manager.loop = loop
+
+loop.set_exception_handler(custom_exception_handler)
 loop.run_forever()
